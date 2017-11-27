6e457f9b627e7965d8d0f37bbbd6acdd3da6d9b8
Matthias Lohscheidt <mail@mlohscheidt.de>
2017-11-27 21:26:05 +0100

    added github.loadLabels

---
+++ src/github/index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const config = require('../config');
+
+
+async function loadLabels(repo) {
+  const labels = await axios.get(
+    `${config.apiUrl}/repos/${repo}/labels`,
+    {
+      headers: {
+        Authorization: `bearer ${config.token}`,
+      },
+    },
+  );
+  return labels.data;
+}
+
+module.exports = { loadLabels };
