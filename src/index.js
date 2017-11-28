c31daa86a7e2878a8e259ae7ce81220c7602d8bf
Matthias Lohscheidt <mail@mlohscheidt.de>
2017-11-28 11:20:20 +0100

    added index.js (first running PoC)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const syncRepo = require('./commands/sync-repo');
+
+const labels = require('./labels.json');
+
+const argv = yargs
+  .usage('gl [command]')
+  .command('sync [target]', 'Sync labels of specified organisation / repository.')
+  .help().argv;
+
+const main = async () => {
+  const command = argv._[0];
+  if (command !== 'sync') {
+    console.log(chalk.red(`${command} is an invalid command`));
+    process.exit();
+  }
+  if(argv.target.match(/\//)) {
+    return syncRepo(argv.target, labels);
+  }
+};
+
+main();
