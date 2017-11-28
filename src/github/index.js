987b2425d7f889093b69d7f3a8b27ed8507716c0
Matthias Lohscheidt <mail@mlohscheidt.de>
2017-11-28 10:24:18 +0100

    added addLabel and updateLabel github functions

--- src/github/index.js
+++ src/github/index.js
@@ -13,4 +13,30 @@ async function loadLabels(repo) {
   return labels.data;
 }
 
-module.exports = { loadLabels };
+async function addLabel(repo, { name, color }) {
+  const labels = await axios.post(
+    `${config.apiUrl}/repos/${repo}/labels`,
+    { name, color },
+    {
+      headers: {
+        Authorization: `bearer ${config.token}`,
+      },
+    },
+  );
+  return labels.data;
+}
+
+async function updateLabel({ url, name, color }) {
+  const labels = await axios.patch(
+    url,
+    { name, color },
+    {
+      headers: {
+        Authorization: `bearer ${config.token}`,
+      },
+    },
+  );
+  return labels.data;
+}
+
+module.exports = { loadLabels, addLabel, updateLabel };
